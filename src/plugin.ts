a9021c731366f0f3ac18781324c85f3ef638e22c
odan <odan3240@gmail.com>
2020-07-09 20:40:43 +0900

    refactor: separate common error handler

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,18 +1,13 @@
 import Vue, { PluginObject } from "vue";
+import { State } from "./state";
+import { Options } from "./options";
+import { createErrorHandler } from "./error-handler";
 
-export type Options = {
-  timeout?: number;
-};
-
-export type State = {
-  value?: Error;
-};
-
-declare module "vue/types/vue" {
-  interface Vue {
-    $error: State;
-  }
-}
+export type VueErrorHandler = (
+  error: Error,
+  vm: Vue,
+  info: string
+) => void | boolean;
 
 export const state = Vue.observable<State>({ value: undefined });
 
@@ -22,24 +17,20 @@ export const plugin: PluginObject<Options> = {
 
     const originalErrorHandler = Vue.config.errorHandler;
 
-    Vue.config.errorHandler = (err, vm, info): void | boolean => {
-      state.value = err;
-
-      if (options?.timeout) {
-        setTimeout(() => {
-          state.value = undefined;
-        }, options?.timeout);
-      }
-
+    const errorHandler: VueErrorHandler = (err, vm, info): void | boolean => {
+      createErrorHandler(state, options)(err);
       if (originalErrorHandler) {
         return originalErrorHandler(err, vm, info);
       }
-
-      if (process.env.NODE_ENV !== "production") {
-        console.error(err);
-      }
-
       return true;
     };
+
+    Vue.config.errorHandler = errorHandler;
   }
 };
+
+declare module "vue/types/vue" {
+  interface Vue {
+    $error: State;
+  }
+}
