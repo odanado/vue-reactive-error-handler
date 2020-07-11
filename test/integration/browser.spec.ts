619d2e6f1a0af9978895ff4c48fcb3b6a767ff0a
odan <odan3240@gmail.com>
2020-07-12 02:28:08 +0900

    test: only chromium

--- test/integration/browser.spec.ts
+++ test/integration/browser.spec.ts
@@ -12,7 +12,8 @@ async function getTextContent(
   return text?.jsonValue();
 }
 
-const browsers = ["webkit", "chromium", "firefox"] as const;
+const browsers = ["chromium"] as const;
+// const browsers = ["webkit", "chromium", "firefox"] as const;
 
 describe.each(browsers)("browser %s", browserName => {
   let server: Server;
